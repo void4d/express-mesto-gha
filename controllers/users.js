644f46af41607ae834ba78882d35fdb1218b78e6
void4d <tulubaevp@icloud.com>
2023-08-24 08:24:24 +0500

    fix: валидация

--- controllers/users.js
+++ controllers/users.js
@@ -21,19 +21,14 @@ function createUser(req, res) {
   return userSchema
     .create({ name, about, avatar })
     .then((r) => res.status(201).send(r))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Неверные данные' });
-      }
-      return res.status(500).send({ message: 'Ошибка сервера' });
-    });
+    .catch(() => res.status(400).send({ message: 'Неверные данные' }));
 }
 
 function updateUser(req, res) {
   const { name, about } = req.body;
 
   return userSchema
-    .findByIdAndUpdate(req.user._id, { name, about }, { new: 'true' })
+    .findByIdAndUpdate(req.user._id, { name, about }, { new: 'true', runValidators: 'true' })
     .then((r) => {
       res.status(200).send(r);
     })
@@ -49,7 +44,7 @@ function updateAvatar(req, res) {
   const { avatar } = req.body;
 
   return userSchema
-    .findByIdAndUpdate(req.user._id, { avatar }, { new: 'true' })
+    .findByIdAndUpdate(req.user._id, { avatar }, { new: 'true', runValidators: 'true' })
     .then((r) => {
       res.status(200).send(r);
     })
